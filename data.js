aa861f57f8b3138947de01e9fdb64bf91f3adae8
peachjelly13 <ananya.sharma2021a@vitstudent.ac.in>
2023-12-29 03:00:22 +0530

    changes

---
+++ data.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id:1,
+        name:'Alena',
+        desc:'I am a dentist',
+    },
+    {
+        id:2,
+        name:'Rita',
+        desc:'I am a teacher',
+    },
+    {
+        id:3,
+        name:'raghav',
+        desc:'I write poetry',
+    },
+]
+const people = [
+    {id:1,name:'Alena'},
+    {id:2,name:'Rita'},
+    {id:3,name:'raghave'},
+]
+module.exports = {products,people}
